41ed9b5c75fc78d81aca379bf3c3876addad5458
Rushabh Mehta <rmehta@gmail.com>
2015-11-04 21:32:46 +0530

    [toc] fix

--- manual_erpnext_com/www/contents/index.py
+++ manual_erpnext_com/www/contents/index.py
@@ -5,7 +5,4 @@ from __future__ import unicode_literals
 from frappe.website.utils import get_full_index
 
 def get_context(context):
-	for item in get_full_index():
-		if item.page_name=="contents":
-			context.full_index = item.children
-			break
+	context.full_index = get_full_index("contents")
